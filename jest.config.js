715d031d5db96750a062254adb595198137d4974
thewanderer313 <ryan.knox13@gmail.com>
2026-06-11 12:30:52 -0400

    Drop unrecognized jest setupFilesAfterEach option

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFilesAfterEach: [],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*|@shopify/react-native-skia|react-native-reanimated|react-native-gesture-handler))',
   ],
