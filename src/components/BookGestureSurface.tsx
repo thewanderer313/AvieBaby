4d93058650ffb760cd719d1254ee7ab22de6a85e
thewanderer313 <ryan.knox13@gmail.com>
2026-06-12 12:01:16 -0400

    Add BookGestureSurface (tap=next, longpress=previous)

---
+++ src/components/BookGestureSurface.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import { runOnJS } from 'react-native-reanimated';
+import { useBook } from '../books/BookProvider';
+
+export const BookGestureSurface: React.FC = () => {
+  const { next, previous } = useBook();
+
+  const tap = Gesture.Tap()
+    .maxDuration(799)
+    .onEnd(() => {
+      runOnJS(next)();
+    });
+
+  const longPress = Gesture.LongPress()
+    .minDuration(800)
+    .maxDistance(40)
+    .shouldCancelWhenOutside(false)
+    .onStart(() => {
+      runOnJS(previous)();
+    });
+
+  const gesture = Gesture.Race(tap, longPress);
+
+  return (
+    <GestureDetector gesture={gesture}>
+      <View style={styles.surface} />
+    </GestureDetector>
+  );
+};
+
+const styles = StyleSheet.create({
+  surface: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: 'transparent',
+  },
+});
